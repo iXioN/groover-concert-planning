d451135556d4a0db393ec816d6ba8c823e0591ad
Antonin Lacombe <alacombe@efficity.com>
2022-02-04 21:50:56 +0100

    implement combination and tests it

--- concert_prog.py
+++ concert_prog.py
@@ -1,16 +1,42 @@
 # concert lenght in minutes
-from typing import Iterable
-
+from typing import FrozenSet, Iterable, Generator, Iterator, List, Tuple, Union
 
 MAXIMUM_NUMBER_OF_TRACKS = 3
 
 
+def combinations(items: Union[List, Tuple], n: int) -> Iterator[Tuple[int]]:
+    """
+    A generator that yield all the unique combination items with n elements
+    Args:
+        items (list or tuple): The sequence of item to combine
+        n (int): The length of elements that will be present in the returned subsequences
+
+    Yields:
+        Iterator[List[int]]: n lenght subsequences of elements combined from items
+    """
+    if n == 0:
+        yield tuple()
+    else:
+        for i in range(len(items)):
+            for sub in combinations(items[i + 1 :], n - 1):
+                item = items[i]
+                item = [item] if isinstance(item, int) else item
+                yield tuple(item) + sub
+
+
 def is_programation_possible(
-    concert_premiere_length: int, track_lenght: Iterable[int]
+    concert_premiere_length: int, track_lenghts: Iterable[int]
 ) -> bool:
     # quicky check the sum of track_lenght
-    total_track_lenght = sum(track_lenght)
+    total_track_lenght = sum(track_lenghts)
     if total_track_lenght <= concert_premiere_length:
         return True
 
+    for possible_tracks in combinations(track_lenghts, MAXIMUM_NUMBER_OF_TRACKS):
+        print(possible_tracks)
+        # do the sum and verify the computation
+        if sum(possible_tracks) == concert_premiere_length:
+            print("good tracks")
+            print(possible_tracks)
+            return True
     return False
--- tests.py
+++ tests.py
@@ -1,5 +1,36 @@
 import unittest
-from concert_prog import is_programation_possible
+from concert_prog import is_programation_possible, combinations
+
+
+class CombinationsTests(unittest.TestCase):
+    def test_combination(self):
+        """
+        given a list of items
+        when i call the combinations function with items and ask for 2 items combination
+        then i get a list of all possible permutations af 2 values
+        """
+        items = (
+            "A",
+            "B",
+            "C",
+        )
+        all_combinations = set(combinations(items, 2))
+        possible_combination = set((("A", "B"), ("A", "C"), ("B", "C")))
+        # both sets should be equals
+        self.assertEqual(all_combinations, possible_combination)
+
+    def test_combination_are_uniques(self):
+        """
+        given a list of 2 items
+        when i call the combinations function with items and ask for 2 items combination
+        then i get a list of 1 possible combination because diplicate are not possible in this implementation
+        """
+        items = (
+            "A",
+            "B",
+        )
+        all_combinations = set(combinations(items, 2))
+        self.assertEqual(len(all_combinations), 1)
 
 
 class IsProgrammationPossibleTests(unittest.TestCase):
@@ -10,8 +41,8 @@ class IsProgrammationPossibleTests(unittest.TestCase):
         Then i get a True
         """
         concert_premiere_length = 7
-        track_lenght = (1, 2, 3)
-        is_possible = is_programation_possible(concert_premiere_length, track_lenght)
+        track_lenghts = (1, 2, 3)
+        is_possible = is_programation_possible(concert_premiere_length, track_lenghts)
         self.assertTrue(is_possible)
 
     def test_track_len_sum_equal_to_concert_premier(self):
@@ -21,8 +52,8 @@ class IsProgrammationPossibleTests(unittest.TestCase):
         Then i get a True
         """
         concert_premiere_length = 6
-        track_lenght = (1, 2, 3)
-        is_possible = is_programation_possible(concert_premiere_length, track_lenght)
+        track_lenghts = (1, 2, 3)
+        is_possible = is_programation_possible(concert_premiere_length, track_lenghts)
         self.assertTrue(is_possible)
 
     def test_smallest_track_len_is_bigger_to_concert_premier(self):
@@ -32,9 +63,20 @@ class IsProgrammationPossibleTests(unittest.TestCase):
         Then i get a True
         """
         concert_premiere_length = 6
-        track_lenght = (8, 9, 10, 11)
-        is_possible = is_programation_possible(concert_premiere_length, track_lenght)
+        track_lenghts = (8, 9, 10, 11)
+        is_possible = is_programation_possible(concert_premiere_length, track_lenghts)
         self.assertFalse(is_possible)
+
+    def test_tracks_fit_the_programation(self):
+        """
+        given a list of 4 length where the sum of 3 of them are equal to the concert_premiere_length
+        when I call is_programation_possible with this list and concert_premiere_length
+        Then i get a True
+        """
+        concert_premiere_length = 9
+        track_lenghts = (2, 5, 6, 7, 3, 4, 8)
+        is_possible = is_programation_possible(concert_premiere_length, track_lenghts)
+        self.assertTrue(is_possible)
 
 
 if __name__ == "__main__":
